900a9d47b17575bb6151f7d91593d22b5e84155d
Thai Pangsakulyanont <dtinth@spacet.me>
2024-08-30 05:18:23 +0700

    add test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,16 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("silverbench", async ({ page }) => {
+  await page.goto("https://silver.urih.com/");
+  await page.locator("#silver").scrollIntoViewIfNeeded();
+  await page.locator("#buttonBenchmark").click();
+  await expect(page.locator("#buttonSubmit")).toBeVisible({
+    timeout: 600000,
+  });
+  const result = await page.locator("#scoreInDone").innerText();
+  test.info().attach("silverbench", {
+    body: result,
+    contentType: "text/plain",
+  });
+  console.log(result);
 });
